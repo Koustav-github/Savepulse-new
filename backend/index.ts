666e16e0a5119a178fc85fe7ee81571caffaa4f1
shramanb113 <shramanb1@gmail.com>
2026-03-07 02:14:06 +0530

    express server added

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { createServer } from "./server";
+
+const PORT = process.env.PORT || 3000;
+
+const app = createServer();
+
+app.listen(PORT, () => {
+  console.log(`🚑 Ambulance backend running on port ${PORT}`);
+});
